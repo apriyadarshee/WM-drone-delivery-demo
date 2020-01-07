942b84b73c093513efdedaf06f5e291b0c4b841c
Anand P <priyadarshee+git@gmail.com>
2020-01-06 23:55:23 -0500

    Updated description, disabled spring annotation for test

--- src/test/java/com/walmartlabs/dronedelivery/wmdrone/WalmartDemoApplicationCommandsTests.java
+++ src/test/java/com/walmartlabs/dronedelivery/wmdrone/WalmartDemoApplicationCommandsTests.java
@@ -3,26 +3,32 @@ package com.walmartlabs.dronedelivery.wmdrone;
 import java.io.IOException;
 import java.util.List;
 
+import com.walmartlabs.dronedelivery.wmdrone.domain.OrderData;
 import com.walmartlabs.dronedelivery.wmdrone.exception.BadInputFileException;
+import com.walmartlabs.dronedelivery.wmdrone.service.DeliveryLaunchCalcualtion;
 import com.walmartlabs.dronedelivery.wmdrone.util.FileReadWriteUtil;
+import com.walmartlabs.dronedelivery.wmdrone.util.InputFileParser;
+
 import static org.hamcrest.Matchers.hasItems;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.hamcrest.core.IsNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WalmartDemoApplicationCommandsTests {
 
-
-	@Test
-	void contextLoads() {
-	}
+	private DeliveryLaunchCalcualtion delCal = new DeliveryLaunchCalcualtion();
+	private InputFileParser parserService = new InputFileParser();
+	// @Test
+	// void contextLoads() {
+	// }
 
 	@Test
 	void testFileRead() {
 
-		final String line = "Hi";
+		final String line = "WM001 N11W5 05:11:50";
 		try {
 			final List<String> parsedLines = FileReadWriteUtil.readFromInputFile("src/main/resources/input.txt");
 			assertThat(parsedLines, IsNull.notNullValue());
@@ -35,8 +41,25 @@ class WalmartDemoApplicationCommandsTests {
 			e.printStackTrace();
 		}
 
+	}
+
+	@Test
+	void testFileConvertedToData() {
+
+		final String line = "WM001 N11W5 05:11:50";
+		try {
+			final List<String> parsedLines = FileReadWriteUtil.readFromInputFile("src/main/resources/input.txt");
+			List<OrderData> inputList = parserService.convertToOrderDataList(parsedLines);
+			assertEquals(parsedLines.size(), inputList.size());
+			assertEquals(inputList.get(0).getId(), "WM001");
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadInputFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
-	
 
 }
